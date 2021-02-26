869f3edd66814fb3ffe1811f6b44e4890e3cb662
GracjanHajdamowicz <gracjan1407@gmail.com>
2021-02-26 14:06:48 +0100

    Zadanie Domowe Z Lekcji Numer 3

--- src/first_week/basics/zadania.java
+++ src/first_week/basics/zadania.java
@@ -3,11 +3,83 @@ package first_week.basics;
 public class zadania {
     public static void main(String[] args) {
 
+// ZADANIE 1
+
+    // Wypisywanie Liczv Całkowitych
+        int myIntA = 14;
+        int myIntB = 7;
+        int myIntC = -3;
+//        System.out.println(myIntA);
+//        System.out.println(myIntB);
+//        System.out.println(myIntC);
+//    // Dodawanie i odejmowanie zmiennych
+//        int sum = myIntA + myIntB = myIntC;
+//        System.out.println("Dodawanie: " + sum);
+//        sum = myIntA - myIntB - myIntC;
+//        System.out.println("Odejmowanie: " + sum);
+//    // Mnożenie zmiennych
+//        sum = myIntA * myIntB * myIntC;
+//        System.out.println("Mnożenie: " + sum);
+//    // Dzielenie zmiennych
+//        sum = myIntA/2;
+//        System.out.println("Dzielenie " + myIntA + "przez 2:" +sum);
+//
+//        sum = myIntB/2;
+//        System.out.println("Dzielenie " + myIntB + "przez 2:" +sum);
+//
+//        sum = myIntC/2;
+//        System.out.println("Dzielenie " + myIntC + "przez 2:" +sum);
+//    // Liczby zmiennoprzecinkowe (double > float)
+//        double myDoubleA = 2.68;
+//        double myDoubleB = 1.42;
+//        double myDoubleC = 0.53;
+//    //Dodawanie double
+//        double sumDouble = myDoubleA + myDoubleB + myDoubleC;
+//        System.out.println("Dodawanie Double: " + sumDouble);
+//    // Mnożenie Double
+//        sumDouble = myDoubleA * myDoubleB * myDoubleC;
+//        System.out.println("Mnożenie Double: " +sumDouble);
+//    // Dzielenie Double
+//        sumDouble = myDoubleA / myDoubleB / myDoubleC;
+//        System.out.println("Dzielenie Double: " + sumDouble);
+//    // Znaki
+//        char mycharA = "G";
+//        char mycharB = "H";
+//        char mycharC = "#";
+//
+//        System.out.println("Moje inicjały to: " + mycharA + mycharB);
+//        System.out.println("Mój ulubiony znak to: " + mycharC);
+//    // Ciągi Znaków
+//        String myStringA = "Gracjan";
+//        String myStringB = "ma";
+//        String myStringC = "BMW";
+//        String myStringD = "E46 Coupe";
+//
+//        String sumString = myStringA + myStringB + myStringC + myStringD;
+//        System.out.println(sumString + "\nI to szybkie");
+//
+//        // KOMENTARZ JEDNOLINIOWY
+//
+//        /*
+//        KOMENTARZ
+//        WIELOLINIOWY
+//         */
+//
+//        // \t TABULATOR
+//
+//        // "\n" ENTER
 
 
+//// ZADANIE 2
+//
+//    int sum = myIntA * myIntA + myIntB * myIntB;
+//        System.out.println("Suma mnożenia potęgi liczby A i B to: " + sum);
 
-
-
-
+//// Zadanie 4
+//
+//        String myStringA = "Gracjan ";
+//        String myStringB = "Hajdamowicz";
+//        String sumString = myStringA + myStringB;
+//        System.out.println("Imię i Nazwisko: " +sumString);
     }
 }
